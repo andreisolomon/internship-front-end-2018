08f75e8e5b048abec2d7064402c7d5dc31ed6b4c
Anca <intern.anca.bodnariuc@assist.ro>
2018-08-27 12:31:57 +0300

    altceva

--- src/app/components/admin/user-list/user-list.component.ts
+++ src/app/components/admin/user-list/user-list.component.ts
@@ -1,23 +1,32 @@
 import {User} from '../user/user.model';
 import {UserListService} from './user-list.service';
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 import {api} from '../../../../assets/data/apiUrl';
+import index from '../../../../../node_modules/@angular/cli/lib/cli';
+import {Observable} from 'rxjs';
+import {Category} from '../../shared/category-list/category';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-
-// tags: ['bla', 'bla']
-export class UserListComponent  {
+export class UserListComponent implements OnInit {
   // public users: User[];
-  // employees: Employee[];
+  // // employees: Employee[];
+  // open = false;
   edit = false;
+  // deleteAll = false;
+  // id: number[];
   // config: Config;
   // public scores: Score[];
-  constructor(private route: Router, private userListService: UserListService) { }
+  public error: boolean = false;
+  public data: Observable<User[]>;
+  public id: number;
+  public link: string;
+  constructor(private route: ActivatedRoute, private userListService: UserListService) {
+  }
 
   // private CourseScoreService
   // ) {this.employees = [{name: 'test'}]; }
@@ -30,54 +39,88 @@ export class UserListComponent  {
   //   {id: 5, email: 'user@assist.ro', firstName: 'trei', lastName: 'patru', password: 'assist', active: 0, image: '../../../../../assets/images/img3.jpeg', tags: 'bla'},
   //   {id: 6, email: 'user@assist.ro', firstName: 'cinci', lastName: 'sase', password: 'assist', active: 1, image: '../../../../../assets/images/img3.jpeg', tags: 'bla'},
   // ];
-  // ngOnInit() {
-  //
-  //   this.users = this.userListService.ge+tData();
-  // // getAllUsers() {
-  //   // return this.users;
-  //
-  // }
-  // delete(us: User) {
-  //   this.users.splice(this.users.indexOf(us), 1);
-  // }
-
-  edity() {
-    if (this.edit === false) {
-      this.edit = true;
-    } else {
-      this.edit = false;
-    }
-  }
-
-  makeAdmin(admin) {
-    admin.isAdmin = admin.isAdmin !== true;
-    // deleteUser(user) {
-    //   (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
-    // }
-
-    //   if (isAdmin == false)
-    //   {
+  ngOnInit() {
+    // this.users = this.userListService.getData();
+    // getAllUsers() {
+    //   return this.users;
     //
-    //   }
-  }
+    this.data = this.userListService.getUsers().map(data => data.slice(0));
 
-  showData() {
-    this.userListService.getInfo().subscribe((response: Response) => {
-        const data = response.json();
-        console.log(data);
+
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['userId'];
+        this.link = '';
+        if (this.userListService.getUserById(this.id)) {
+          this.error = params['userId'] != null;
+        }
       }
     );
+
+    if (this.error === true) {
+      this.link = '../../';
+      setTimeout(() => {
+        this.id = 0;
+        this.error = false;
+      }, 10000);
+    }
+
   }
-}
-
+  }
+// openMenu() {
+//     this.open = this.open === false;
 // }
-// ngOnInit() {
-//   this.getAllUsers();
+//   delete(us: User) {
+//     this.users.splice(this.users.indexOf(us), 1);
+//   }
+//
+//   edity() {
+//     if (this.edit === false) {
+//       this.edit = true;
+//     } else {
+//       this.edit = false;
+//     }
+//   }
+//
+//   makeAdmin(admin) {
+//     admin.isAdmin = admin.isAdmin !== true;
+//     // admin.isAdmin = admin.isAdmin !== true;
+//     // deleteUser(user){
+//     //   (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+//     // }
+//     //
+//     //   if (isAdmin === false)
+//     //   {
+//     //
+//     //   }
+//   }
+//
+//   //
+//   // showData() {
+//   //   this.userListService.getInfo().subscribe((response: Response) => {
+//   //       const data = response.json();
+//   //       console.log(data);
+//   //     }
+//   //   );
+//   // }
+//   // passId(index: number) {
+//   //   this.id.push(index);
+//   // }
+//   check(e) {
+//     // this.deleteAll = !!e.target.checked;
+//     if (e.target.checked) {
+//       this.deleteAll = true;
+//       // this.passId.push(index);
+//     } else {
+//       this.deleteAll = false;
+//       // this.passId.delete(index);
+//     }
+//   }
+//   showButton() {
+//     if (document.getElementById('checked').valueOf() === true) {
+//       document.getElementById('ok').innerHTML = 'asa';
+//     }
+//     this.deleteAll = true;
+//   }
 // }
-// private userList
-
-
-export class Employee {
-  name: string;
-}
 
--- src/app/components/admin/user/user.model.ts
+++ src/app/components/admin/user/user.model.ts
@@ -1,90 +1,86 @@
 export class User {
-private isAdmin: boolean;
-  private id: number;
-  private image: string;
-  private firstname: string;
-  private tags: string;
-  private date: string;
-  private score: number;
-  private lastname: string;
-  private email: string;
-  private password: string;
-
-  constructor(isAdmin: boolean, id: number, image: string, firstname: string, lastname: string, email: string, password: string, tags: string, score: number, date: string) {
-    this.setAdmin(isAdmin);
-    this.setId(id);
-    this.setScore(score);
-    this.setImage(image);
-    this.setFirstName(firstname);
-    this.setLastName(lastname);
-    this.setEmail(email);
-    this.setPassword(password);
-    this.setTags(tags);
-    this.setDate(date);
-
-  }
-  setAdmin(isAdmin: boolean) {
-    this.isAdmin = isAdmin;
-  }
-  setScore(score: number) {
-    this.score = score;
-  }
-  setId(id: number) {
-    this.id = id;
-  }
-
-  setImage(image: string) {
-    this.image = image;
-  }
-  setFirstName(firstname: string) {
-    this.firstname = firstname;
-  }
-  setLastName(lastname: string) {
-    this.lastname = lastname;
-  }
-  setEmail(email: string) {
-    this.email = email;
-  }
-  setPassword(password: string) {
-    this.password = password;
-  }
-  setTags(tags: string) {
-    this.tags = tags;
-  }
-  setDate(date: string) {
-    this.date = date;
-  }
-
-  getAdmin() {
-    return this.isAdmin;
-  }
-  getId() {
-    return this.id;
-  }
-  getScore() {
-    return this.score;
-  }
-  getImage() {
-    return this.image;
-  }
-  getFirstName() {
-    return this.firstname;
-  }
-  getLastName() {
-    return this.lastname;
-  }
-  getEmail() {
-    return this.email;
-  }
-  getPassword() {
-    return this.password;
-  }
-  getTags() {
-    return this.tags;
-  }
-  getDate() {
-    return this.date;
-  }
+  id: number;
+  FirstName: string;
+  LastName: string;
+  Mail: string;
+  Admin: boolean;
+  Points: number;
+  createdAt: string;
+  // constructor(isAdmin: boolean, id: number, image: string, firstname: string, lastname: string, email: string, password: string, tags: string, score: number, date: string) {
+  //   this.setAdmin(isAdmin);
+  //   this.setId(id);
+  //   this.setScore(score);
+  //   this.setImage(image);
+  //   this.setFirstName(firstname);
+  //   this.setLastName(lastname);
+  //   this.setEmail(email);
+  //   this.setPassword(password);
+  //   this.setTags(tags);
+  //   this.setDate(date);
+  //
+  // }
+  // setAdmin(isAdmin: boolean) {
+  //   this.isAdmin = isAdmin;
+  // }
+  // setScore(score: number) {
+  //   this.score = score;
+  // }
+  // setId(id: number) {
+  //   this.id = id;
+  // }
+  //
+  // setImage(image: string) {
+  //   this.image = image;
+  // }
+  // setFirstName(firstname: string) {
+  //   this.firstname = firstname;
+  // }
+  // setLastName(lastname: string) {
+  //   this.lastname = lastname;
+  // }
+  // setEmail(email: string) {
+  //   this.email = email;
+  // }
+  // setPassword(password: string) {
+  //   this.password = password;
+  // }
+  // setTags(tags: string) {
+  //   this.tags = tags;
+  // }
+  // setDate(date: string) {
+  //   this.date = date;
+  // }
+  //
+  // getAdmin() {
+  //   return this.isAdmin;
+  // }
+  // getId() {
+  //   return this.id;
+  // }
+  // getScore() {
+  //   return this.score;
+  // }
+  // getImage() {
+  //   return this.image;
+  // }
+  // getFirstName() {
+  //   return this.firstname;
+  // }
+  // getLastName() {
+  //   return this.lastname;
+  // }
+  // getEmail() {
+  //   return this.email;
+  // }
+  // getPassword() {
+  //   return this.password;
+  // }
+  // getTags() {
+  //   return this.tags;
+  // }
+  // getDate() {
+  //   return this.date;
+  // }
 
 
 
