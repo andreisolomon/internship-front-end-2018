b303d72ad0489ea76adf97b16a0739fe6ab65aee
Anca <intern.anca.bodnariuc@assist.ro>
2018-08-27 18:01:10 +0300

    edit

--- src/app/components/admin/user-list/user-element/user-element.component.ts
+++ src/app/components/admin/user-list/user-element/user-element.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../../user/user.model';
+import { UserListService } from '../user-list.service';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-user-element',
@@ -7,11 +9,20 @@ import {User} from '../../user/user.model';
   styleUrls: ['./user-element.component.css']
 })
 export class UserElementComponent implements OnInit {
-  @Input() user: User[];
+  public user;
+  public id;
 
-  constructor() { }
+  constructor(private userList: UserListService, private route: ActivatedRoute) {  }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+      }
+    );
+
+    this.userList.getUserById(this.id).subscribe(data => console.log(this.user = data));
+
   }
 
 }
